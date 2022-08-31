97c14a631db4fdb66b1ab383ca7e8c5410cda745
Usuario <Usuario@DESKTOP-48TUMLC>
2022-08-30 21:04:44 -0500

    Agregar archivos de proyecto.

---
+++ Complejo.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OperacionComplejosPOO
+{
+    class Complejo
+    {
+        private double partReal, partImaginary;
+        /// Complejo compoundResult = new Complejo();
+
+        public double PartReal { get => partReal; set => partReal = value; }
+        public double PartImaginary { get => partImaginary; set => partImaginary = value; }
+
+        public Complejo()
+        {
+            partReal = 0;
+            partImaginary = 0;
+        }
+
+        public Complejo(double partReal, double partImaginary)
+        {
+            this.partReal = partReal;
+            this.partImaginary = partImaginary;
+        }
+
+        public override string ToString()
+        {
+            return $"Imaginary: { partImaginary }, Int: { partReal }";
+        }
+
+        /// Métodos
+
+        public static Complejo operator +(Complejo i1, Complejo i2)
+        {
+            double e = 0;
+            double i = 0;
+
+            e = i1.partReal + i2.partReal;
+            i = i1.partImaginary + i2.partImaginary;
+
+            Complejo resultadoComplejo = new Complejo(e, i);
+            return resultadoComplejo;
+        }
+
+        public static Complejo operator -(Complejo i1, Complejo i2)
+        {
+            double e = 0;
+            double i = 0;
+
+            e = i1.partReal - i2.partReal;
+            i = i1.partImaginary - i2.partImaginary;
+
+            Complejo resultadoComplejo = new Complejo(e, i);
+            return resultadoComplejo;
+        }
+
+        public static Complejo operator *(Complejo i1, Complejo i2)
+        {
+            double e = 0;
+            double i = 0;
+
+            e = (i1.partReal * i2.partReal) - (i1.partImaginary * i2.partImaginary);
+            i = (i1.partReal * i2.partImaginary) + (i1.partImaginary * i2.partReal);
+
+            Complejo resultadoComplejo = new Complejo(e, i);
+            return resultadoComplejo;
+        }
+
+        public static Complejo operator *(Complejo i1, double n)
+        { 
+
+            double e = i1.partReal * n;
+            double i = i1.partImaginary * n;
+
+            Complejo resultadoComplejo = new Complejo(e, i);
+            return resultadoComplejo;
+        }
+
+        public static Complejo operator /(Complejo i1, Complejo i2)
+        {
+            double e = 0;
+            double i = 0;
+
+            e = ((i1.partReal * i2.partReal) / (Math.Pow(i2.partReal, 2) + Math.Pow(i2.partImaginary, 2)));
+            i = (i1.partImaginary * i2.partReal - i1.partReal * i2.partImaginary) / ((Math.Pow(i2.partReal, 2) + Math.Pow(i2.partImaginary, 2)));
+
+            Complejo resultadoComplejo = new Complejo(e, i);
+            return resultadoComplejo;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OperacionComplejosPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Complejo i1 = new Complejo(10, 6);
+            Complejo i2 = new Complejo(8, 1);
+
+            Console.WriteLine($"Suma: { i1 + i2 }");
+            Console.WriteLine($"Resta: { i1 + i2 }");
+            Console.WriteLine($"Multiplicación: { i1 + i2 }");
+            Console.WriteLine($"Multiplicación con un Double: { i1 * 2.5 }");
+            Console.WriteLine($"División: { i1 / i2 }");
+
+
+        }
+    }
+}
